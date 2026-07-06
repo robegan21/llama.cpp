6d24be9606d27b9d7fe34a04d4c7e35bcb6ab645
rob <rob>
2026-07-06 09:17:29 -0700

    fixed endless loop

--- tools/server/server-context.cpp
+++ tools/server/server-context.cpp
@@ -17,6 +17,8 @@
 #include "mtmd.h"
 #include "mtmd-helper.h"
 
+#include "../src/llama-kv-cache-iswa.h"
+
 #include <algorithm>
 #include <cstddef>
 #include <cinttypes>
@@ -3253,7 +3255,15 @@ private:
                             const auto pos_min_thold = std::max(0, pos_next - n_swa - (has_new_tokens ? 0 : 1));
 
                             if (n_past > 0 && n_past <= slot.prompt.n_tokens()) {
-                                const auto pos_min = llama_memory_seq_pos_min(llama_get_memory(ctx_tgt), slot.id);
+                                llama_pos pos_min;
+                                {
+                                    auto * kv_iswa = dynamic_cast<llama_kv_cache_iswa *>(llama_get_memory(ctx_tgt));
+                                    if (kv_iswa) {
+                                        pos_min = kv_iswa->get_swa()->seq_pos_min(slot.id);
+                                    } else {
+                                        pos_min = llama_memory_seq_pos_min(llama_get_memory(ctx_tgt), slot.id);
+                                    }
+                                }
                                 if (pos_min == -1) {
                                     SLT_ERR(slot, "n_past = %d, slot.prompt.tokens.size() = %d, seq_id = %d, pos_min = %d\n", n_past, (int) slot.prompt.tokens.size(), slot.id, pos_min);
                                     GGML_ABORT("pos_min == -1, but n_past > 0 - should not happen: https://github.com/ggml-org/llama.cpp/pull/13833#discussion_r2116181237");
